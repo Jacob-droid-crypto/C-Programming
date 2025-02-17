ce12cf9264f1dbe2914cee0ce81b44b6fb6fc10d
Jacob Thomas <jacobthomassjcet@gmail.com>
2025-02-17 22:28:19 +0530

    Create Multiplication_Table.c

---
+++ Multiplication_Table.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int num;
+    printf("Enter the number: ");
+    scanf("%d",&num);
+    for(int i=0;i<=10;i++)
+    {
+        printf("%d * %d = %d \n",num,i,num*i);
+    }
+    return 0;
+}
+    
